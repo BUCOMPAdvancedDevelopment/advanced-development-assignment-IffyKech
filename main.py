78a654d5d6e6ece71b531156ba32e4d719c3ad61
iffykech@gmail.com <iffykech@gmail.com>
2021-12-07 10:59:19 +0000

    Firebase authentication

--- main.py
+++ main.py
@@ -3,25 +3,37 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/static/css/<file>')
-def return_css(file):
-    return app.send_static_file(file)
-
-
 @app.route('/')
 def login_redirect():
     return redirect(url_for('render_login'))
-
 
 @app.route('/login')
 def render_login():
     return render_template('login.html')
 
-
+""" USER SECTION """
 @app.route('/home')
 def render_index():
     return render_template('index.html')
 
+@app.route('/tracking')
+def render_tracking():
+    return render_template('tracking.html')
+
+@app.route('/products')
+def render_products():
+    return render_template('products.html')
+
+@app.route('orders')
+def render_orders():
+    return render_template('orders.html')
+
+"""ADMIN SECTION """
+@app.route('/admin')
+def render_admin():
+    return render_template('admin.html')
+
+""" ERROR HANDLERS """
 @app.errorhandler(500)
 def server_error(e):
     logging.exception('An error occurred during a request')

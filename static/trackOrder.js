379f703ee1b232c0738778014587136f1908c0a8
iffykech@gmail.com <iffykech@gmail.com>
2021-12-14 23:01:26 +0000

    Implement check for admin account

--- static/trackOrder.js
+++ static/trackOrder.js
@@ -3,11 +3,9 @@
 window.addEventListener('load', function(){
     firebase.auth().onAuthStateChanged(function(user){
         document.getElementById('formSubmit').onclick = function() {
-            console.log("Worked");
             var order_number = document.getElementById('trackingInput').value;
             var postBody = {"order_number": order_number};
             
-
             // Start HTTP request
             var xhr = new XMLHttpRequest();
             xhr.open("POST", '/track', true); // asynchronous
@@ -16,7 +14,12 @@ window.addEventListener('load', function(){
                 if (xhr.readyState === 4) {
                     if (xhr.status === 200) {
                         var response = JSON.parse(xhr.responseText);
-                        window.location.assign('/orders?id=' + response['order_id']);
+                        if (user.email === "admin@hotmail.co.uk") { // admin
+                            window.location.assign('/admin/orders?id=' + response['order_id']);
+                        }
+                        else {
+                            window.location.assign('/orders?id=' + response['order_id']);
+                        }
                     }
                     if (xhr.status === 400) {
                         alert("Incorrect order number. Please try again");

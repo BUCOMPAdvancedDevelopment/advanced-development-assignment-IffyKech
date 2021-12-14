c84c5abdeae8a6e4b805626ec94e0b79a49e1241
iffykech@gmail.com <iffykech@gmail.com>
2021-12-14 23:06:25 +0000

    Edit Order Script

---
+++ static/adminOrderEdit.js
@@ -0,0 +1,54 @@
+'use strict';
+
+window.addEventListener('load', function(){
+    firebase.auth().onAuthStateChanged(function(user){
+        document.getElementById('saveChanges').onclick = function() {
+            var status = document.getElementById('statusInput').value;
+            var trackPoint = document.getElementById('trackPointInput').value;
+            var estimatedDelivery = document.getElementById('estimatedDeliveryInput').value;
+            var deliveryAddress = document.getElementById('deliveryAddressInput').value;
+            var order_id = document.getElementById('orderIDText').innerText;
+
+            if (status.length < 1 || trackPoint.length < 1 || estimatedDelivery.length < 1 || deliveryAddress.length < 1) {
+                alert("Please fill all fields before saving changes");
+            }
+
+            else {
+                var postBody = {
+                    "order_id": order_id,
+                    "status": status,
+                    "track_point": trackPoint,
+                    "estimated_delivery": estimatedDelivery,
+                    "delivery_address": deliveryAddress
+                };
+
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", '/admin/update', true); // asynchronous
+                xhr.setRequestHeader('Content-Type', 'application/json'); // indicates body of post is JSON data
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === 4) {
+                        if (xhr.status === 200) {
+                            alert("Order Updated!\nRefresh page to see changes");
+                        }
+                        
+                    }
+                }
+                xhr.send(JSON.stringify(postBody));
+            }
+        },
+        document.getElementById('deleteOrder').onclick = function() {
+            var order_id = document.getElementById('orderIDText').innerText;
+
+            var xhr = new XMLHttpRequest();
+                xhr.open("DELETE", '/admin/delete?id=' + order_id, true); // asynchronous
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === 4) {
+                        if (xhr.status === 200) {
+                            alert("Order Deleted!\nRefresh page to see changes");
+                        }
+                    }
+                }
+                xhr.send();
+        }
+    });
+});
